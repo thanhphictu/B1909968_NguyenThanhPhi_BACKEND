bdaf374bacabb8bd0f106780f772bfb1db4bcf9c
thanhphictu <thanhphi107610@gmail.com>
2023-02-18 13:11:38 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const ApiError = require("./app/api-error");
 
 const contactsRouter = require("./app/routes/contact.route");
 
@@ -8,6 +9,17 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api/contacts", contactsRouter);
+
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((err, req, res, next) => {
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error",
+    });
+});
+
 
 app.get("/", (rep, res) => {
     res.json({ message: "Welcome to contact book application." });
